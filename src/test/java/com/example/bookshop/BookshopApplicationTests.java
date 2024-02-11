ee6c239a0b7e17914525cf8e129da3a17945a100
lubop <lubopuschenreiter@gmail.com>
2024-02-11 09:27:30 +0100

    test

--- src/test/java/com/example/bookshop/BookshopApplicationTests.java
+++ src/test/java/com/example/bookshop/BookshopApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.bookshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BookshopApplicationTests.class)
 class BookshopApplicationTests {
 
 	@Test
